05e4b770b5022cbd3dafb8d2eb553097ecbb0404
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-09-28 11:35:27 -0500

    Initial Commit

---
+++ MauiReversi/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Windows.Input;
+
+namespace MauiReversi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TileClicked(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            var param = button.CommandParameter as String;
+            Debug.WriteLine(param);
+        }
+    }
+}
---
+++ MauiReversi/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiReversi
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
