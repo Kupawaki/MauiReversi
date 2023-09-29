561fcca05e3f558e49ec6fd833351b102b109403
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-09-28 22:01:03 -0500

    Working reset button

--- MauiReversi/MainPage.xaml.cs
+++ MauiReversi/MainPage.xaml.cs
@@ -8,7 +8,7 @@ namespace MauiReversi
         private int turn = 0;
         private Color[] colors = { Color.FromRgb(255,50,50), Color.FromRgb(50,255,50) };
 
-        private Button[][] tiles;
+        private Button[,] tiles = new Button[3,3];
 
         public MainPage()
         {
@@ -20,6 +20,18 @@ namespace MauiReversi
         private void fillTileArray()
         {
             Debug.WriteLine(grid.Children.ToArray());
+
+            tiles[0,0] = b00;
+            tiles[0,1] = b01;
+            tiles[0,2] = b02;
+
+            tiles[1,0] = b10;
+            tiles[1,1] = b11;
+            tiles[1,2] = b12;
+
+            tiles[2,0] = b20;
+            tiles[2,1] = b21;
+            tiles[2,2] = b22;
         }
 
         private void update()
@@ -30,9 +42,8 @@ namespace MauiReversi
         private void TileClicked(object sender, EventArgs e)
         {
 
-            Debug.WriteLine(grid.Children.ToArray());
             Button button = sender as Button;
-            Debug.WriteLine(grid.GetColumn(button));
+            //Debug.WriteLine(grid.GetColumn(button));
             //Debug.WriteLine(grid.GetRow(button));
 
             if (button.Text == "")
@@ -40,19 +51,29 @@ namespace MauiReversi
                 if (turn == 0)
                 {
                     button.BackgroundColor = colors[turn];
-                    button.Text = colors[turn].ToString();
+                    button.Text = " ";
                     turn = 1;
                 }
                 else
                 {
                     button.BackgroundColor = colors[turn];
-                    button.Text = colors[turn].ToString();
+                    button.Text = " ";
                     turn = 0;
                 }
             }
             else
             {
                 Debug.WriteLine("You cant play here");
+            }
+        }
+
+        private void ResetGame(object sender, EventArgs e)
+        {
+            foreach (Button b in tiles)
+            {
+                b.Text = "";
+                b.BackgroundColor = Color.FromRgb(255, 255, 255);
+                turn = 0;
             }
         }
     }
